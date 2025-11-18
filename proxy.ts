79adbbdb921f476bff9c415f554aeabe874213a5
OsamaImtiaz-Napollo <osama@napollo.online>
2025-11-18 23:48:10 +0500

    What metadata is.
    Types of metadata.
    
    How to add an Open Graph image using metadata.
    
    How to add a favicon using metadata.

--- proxy.ts
+++ proxy.ts
@@ -1,12 +1,12 @@
-import NextAuth from "next-auth"
-import { authConfig } from "./auth.config"
+import { auth } from "./auth"
 
-export default NextAuth(authConfig).auth
-
+export const proxy = auth()
+export { proxy as default }
 export const config = {
-  // https://nextjs.org/docs/app/api-reference/file-conventions/proxy#matcher
-  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+  // https://nextjs.org/docs/app/api-reference/next-config-js/proxy#config
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
 }
-
 //The advantage of employing Proxy for this task is that the protected routes will not even start rendering
 // until the Proxy verifies the authentication, enhancing both the security and performance of your application.
